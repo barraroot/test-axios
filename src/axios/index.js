0901d1f286cd57688951c53081777d9125b10dbb
Lucas Carvalho <barraroot@gmail.com>
2023-08-07 21:49:24 -0300

    Rodando requisção get

---
+++ src/axios/index.js
@@ -0,0 +1,24 @@
+import axios from 'axios'
+
+axios.defaults.baseURL = '/api'
+//axios.defaults.withCredentials = true
+axios.defaults.headers.common['Content-Type'] = 'application/json';
+axios.defaults.headers.common['EVEREST-DASH'] = 'demo.everestgames.com.br';
+axios.defaults.headers.common['crossDomain'] = true
+
+
+
+// Adiciona um interceptador na requisição
+axios.interceptors.request.use(function (config) {
+    // Faz alguma coisa antes da requisição ser enviada
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers["Authorization"] = "Bearer " + token;
+    }
+    return config;
+  }, function (error) {
+    // Faz alguma coisa com o erro da requisição
+    console.log(error)
+    return Promise.reject(error);
+  });
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'https://backoffice.everestgames.com.br/',
+        changeOrigin: true,
+        headers: {
+          'host': 'demo.everestgames.com.br',
+          'EVEREST-DASH': 'demo.everestgames.com.br'
+        }
+      }
+    }
+  }  
 })
