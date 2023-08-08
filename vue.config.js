5146b0fe62ae28df65d6a531bc11b3c2a6f8da10
Lucas Carvalho <barraroot@gmail.com>
2023-08-08 10:57:07 -0300

    Correção axios #3

--- vue.config.js
+++ vue.config.js
@@ -9,7 +9,6 @@ module.exports = defineConfig({
         target: 'https://backoffice.everestgames.com.br/',
         changeOrigin: true,
         headers: {
-          'host': 'demo.everestgames.com.br',
           'EVEREST-DASH': 'demo.everestgames.com.br'
         }
       }
